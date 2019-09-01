848cbdf53d00f75d2dac340fd97b780743b6e297
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-01 18:53:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_to_all_true (source_array)
   counter = 0 
   while counter < source_array.size do
-  total += source_array [counter]
+  return false if 
   counter += 1
   end
   return total
