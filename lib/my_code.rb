3a4eb565e093f4c0499237312dd43b950a57231a
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-01 18:34:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,13 @@ def map_to_no_change (source_array)
   return array_done
 end
   
+def map_to_negativize (source_array)
+  array_done = []
+  counter = 0 
+  while counter < source_array.size do
+  array_done.push (source_array[counter] *2)
+  counter += 1
+  end
+  return array_done
+end
+
