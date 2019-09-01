bca762bd9da2d5e2b0a846764e12b2cbc370e69d
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-01 18:46:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,7 @@ def reduce_to_total (source_array, starting_point=0)
   total = starting_point
   counter = 0 
   while counter < source_array.size do
-  total += source_array []
+  total += source_array [counter]
   counter += 1
   end
   return total
