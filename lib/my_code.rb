35cd089346b829a0ac600b6d1cd0708b9f9f9554
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-01 18:32:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,12 @@ def map_to_negativize (source_array)
 end
 
 def map_to_no_change (source_array)
-  dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
   array_done = []
-  counter = 0
-  while counter < 
+  counter = 0 
+  while counter < source_array.size do
+  array_done.push (source_array[counter] *-1)
+  counter += 1
+  end
+  return array_done
 end
   
