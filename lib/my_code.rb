d9f3fbab21e57bb9a825fcf8d73d1a7ca2e0b4c7
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-01 18:33:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def map_to_no_change (source_array)
   array_done = []
   counter = 0 
   while counter < source_array.size do
-  array_done.push (source_array[counter] *-1)
+  array_done.push (source_array[counter])
   counter += 1
   end
   return array_done
